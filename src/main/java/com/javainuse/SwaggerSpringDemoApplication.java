b35a8c7f022da7fc70921c12998b50b84be61e84
kwak kil hun <windmoter@gmail.com>
2024-09-13 10:17:05 +0900

    test

--- src/main/java/com/javainuse/SwaggerSpringDemoApplication.java
+++ src/main/java/com/javainuse/SwaggerSpringDemoApplication.java
@@ -2,7 +2,7 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class SwaggerSpringDemoApplication {
 
